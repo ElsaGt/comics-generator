d5a5452491cc04b0de5cca92b42a0aee1c441829
Elsa Gounot <elsa.gounot@gmail.com>
2018-05-07 16:41:25 +0200

    clean db

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,29 +9,29 @@
 puts 'Creating strips...'
 
 amazing_adventure_funnies = Strip.create(
-  start_img: 'amazing_adventure_funnies_1.jpg'
-  middle_img: 'amazing_adventure_funnies_2.jpg'
-  end_img: 'amazing_adventure_funnies_3.jpg'
-  title: 'Amazing Adventure Funnies 1'
-  author: 'unknown'
+  start_img: 'amazing_adventure_funnies_1.jpg',
+  middle_img: 'amazing_adventure_funnies_2.jpg',
+  end_img: 'amazing_adventure_funnies_3.jpg',
+  title: 'Amazing Adventure Funnies 1',
+  author: 'unknown',
   date: DateTime.new(1940,6,1)
 )
 
 boy_comics = Strip.create(
-  start_img: 'boy_comics_1.jpg'
-  middle_img: 'boy_comics_2.jpg'
-  end_img: 'boy_comics_3.jpg'
-  title: 'Boy Comics 18'
-  author: 'unknown'
+  start_img: 'boy_comics_1.jpg',
+  middle_img: 'boy_comics_2.jpg',
+  end_img: 'boy_comics_3.jpg',
+  title: 'Boy Comics 18',
+  author: 'unknown',
   date: DateTime.new(1944,10,1)
 )
 
 brenda_starr = Strip.create(
-  start_img: 'brenda_starr_1.jpg'
-  middle_img: 'brenda_starr_2.jpg'
-  end_img: 'brenda_starr_3.jpg'
-  title: 'Brenda Starr 3'
-  author: 'unknown'
+  start_img: 'brenda_starr_1.jpg',
+  middle_img: 'brenda_starr_2.jpg',
+  end_img: 'brenda_starr_3.jpg',
+  title: 'Brenda Starr 3',
+  author: 'unknown',
   date: DateTime.new(1948,06,1)
 )
 
