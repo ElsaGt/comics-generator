e9ae55eb30fa8cbbc46e73599d659ca74d2cf31a
Elsa Gounot <elsa.gounot@gmail.com>
2018-05-21 21:59:59 +0200

    beggining of popup confirmation

--- app/controllers/strips_controller.rb
+++ app/controllers/strips_controller.rb
@@ -9,15 +9,21 @@ class StripsController < ApplicationController
   end
 
   def create
-    @strip = Strip.create(strip_params)
-    redirect_to strips_path
+    @strip = Strip.new(strip_params)
+
+    if @strip.save
+      redirect_to '/', notice: 'The strip has been successfully added to the database. Thank you!'
+    else
+      redirect_to '/', notice: 'Please try again and be sure to upload 3 panels.'
+    end
   end
 
 
   private
 
   def strip_params
-    params.require(:strip).permit(:start_img, :middle_img, :end_img, :title, :author, :date, :year)
+    params.require(:strip).permit(:start_img, :middle_img, :end_img, :title, :author, :date, :year, :notice)
   end
 
 end
+
